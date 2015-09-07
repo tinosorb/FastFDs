132427304becaccf0620327205c099578186adb6
Xiao (Shawn) Meng <meng14fall@gmail.com>
2015-09-07 01:49:37 -0400

    DB support.

--- src/algorithm/test.java
+++ src/algorithm/test.java
@@ -102,55 +102,55 @@ public class test {
 //					+ table
 //					+ "';
 			
-			String add_id = "alter fastfd add id SERIAL";
+			String add_id = "alter table fastfd add id SERIAL";
 			 
-			String inner_join = "CREATE VIEW joined AS"
-					+ "SELECT t1.a AS A1, t2.a AS A2, t1.b AS B1, t2.b AS B2, t1.c AS C1, t2.c AS C2, t1.d AS D1, t2.d AS D2,"
-					+ "t1.e AS E1, t2.e AS E2, t1.f AS F1, t2.f AS F2"
-					+ "FROM fastfd t1"
-					+ "INNER JOIN fastfd t2"
+			String inner_join = "CREATE VIEW joined AS "
+					+ "SELECT t1.id AS id, t1.a AS A1, t2.a AS A2, t1.b AS B1, t2.b AS B2, t1.c AS C1, t2.c AS C2, t1.d AS D1, t2.d AS D2, "
+					+ "t1.e AS E1, t2.e AS E2, t1.f AS F1, t2.f AS F2 "
+					+ "FROM fastfd t1 "
+					+ "INNER JOIN fastfd t2 "
 					+ "ON t1.id <t2.id";
 			
-			String diff_set = "CREATE VIEW diffset AS"
-					+ "SELECT id, 'a' AS Diff"
-					+ "FROM ("
-					+ "SELECT id, a1, a2 FROM joined"
-					+ "WHERE a1 <> a2"
-					+ ") AS Diffs"
-					+ "UNION"
-					+ "SELECT id, 'b' AS Diff"
-					+ "FROM ("
-					+ "SELECT id, b1, b2 FROM joined"
-					+ "WHERE b1 <> b2"
-					+ ") AS Diffs"
-					+ "UNION"
-					+ "SELECT id, 'c' AS Diff"
-					+ "FROM ("
-					+ "SELECT id, c1, c2 FROM joined"
-					+ "WHERE c1 <> c2"
-					+ ") AS Diffs"
-					+ "UNION"
-					+ "SELECT id, 'd' AS Diff"
-					+ "FROM ("
-					+ "SELECT id, d1, d2 FROM joined"
-					+ "WHERE d1 <> d2"
-					+ ") AS Diffs"
-					+ "UNION"
-					+ "SELECT id, 'e' AS Diff"
-					+ "FROM ("
-					+ "SELECT id, e1, e2 FROM joined"
-					+ "WHERE e1 <> e2"
-					+ ") AS Diffs"
-					+ "UNION"
-					+ "SELECT id, 'f' AS Diff"
-					+ "FROM ("
-					+ "SELECT id, f1, f2 FROM joined" 
-					+ "WHERE f1 <> f2"
+			String diff_set = "CREATE VIEW diffset AS "
+					+ "SELECT id, 'a' AS Diff "
+					+ "FROM ( "
+					+ "SELECT id, a1, a2 FROM joined "
+					+ "WHERE a1 <> a2 "
+					+ ") AS Diffs "
+					+ "UNION "
+					+ "SELECT id, 'b' AS Diff "
+					+ "FROM ( "
+					+ "SELECT id, b1, b2 FROM joined "
+					+ "WHERE b1 <> b2 "
+					+ ") AS Diffs "
+					+ "UNION "
+					+ "SELECT id, 'c' AS Diff "
+					+ "FROM ( "
+					+ "SELECT id, c1, c2 FROM joined "
+					+ "WHERE c1 <> c2 "
+					+ ") AS Diffs "
+					+ "UNION "
+					+ "SELECT id, 'd' AS Diff "
+					+ "FROM ( "
+					+ "SELECT id, d1, d2 FROM joined "
+					+ "WHERE d1 <> d2 "
+					+ ") AS Diffs "
+					+ "UNION "
+					+ "SELECT id, 'e' AS Diff "
+					+ "FROM ( "
+					+ "SELECT id, e1, e2 FROM joined "
+					+ "WHERE e1 <> e2 "
+					+ ") AS Diffs "
+					+ "UNION "
+					+ "SELECT id, 'f' AS Diff "
+					+ "FROM ( "
+					+ "SELECT id, f1, f2 FROM joined " 
+					+ "WHERE f1 <> f2 "
 					+ ") AS Diffs";
 					
-			String diffset_output = "CREATE VIEW diffset_output AS"
-					+ "SELECT id, array_agg(diff) AS DifferenceSets"
-					+ "FROM diffset"
+			String diffset_output = "CREATE VIEW diffset_output AS "
+					+ "SELECT id, array_agg(diff) AS DifferenceSets "
+					+ "FROM diffset "
 					+ "GROUP BY id";
 			
 			String receive_diffset = "SELECT * FROM diffset_output";
@@ -158,35 +158,35 @@ public class test {
 			long start1 = System.currentTimeMillis();
 			Statement st1 = connection.createStatement();
 			st1.executeUpdate(add_id);
-			System.out.println(add_id);
+//			System.out.println(add_id);
 			System.out.println("ID column added");
 			query_time = query_time + (System.currentTimeMillis() - start1);
 			
 			long start2 = System.currentTimeMillis();
 			Statement st2 = connection.createStatement();
 			st2.executeUpdate(inner_join);
-			System.out.println(inner_join);
+//			System.out.println(inner_join);
 			System.out.println("Joined view created");
 			query_time = query_time + (System.currentTimeMillis() - start2);
 			
 			long start3 = System.currentTimeMillis();
 			Statement st3 = connection.createStatement();
 			st3.executeUpdate(diff_set);
-			System.out.println(diff_set);
+//			System.out.println(diff_set);
 			System.out.println("Differences spotted");
 			query_time = query_time + (System.currentTimeMillis() - start3);
 			
 			long start4 = System.currentTimeMillis();
 			Statement st4 = connection.createStatement();
 			st4.executeUpdate(diffset_output);
-			System.out.println(diffset_output);
+//			System.out.println(diffset_output);
 			System.out.println("Diffset view created");
 			query_time = query_time + (System.currentTimeMillis() - start4);
 			
 			long start5 = System.currentTimeMillis();
 			Statement st5 = connection.createStatement();
 			ResultSet rs = st5.executeQuery(receive_diffset);
-			System.out.println(receive_diffset);
+			System.out.println("Diffset received!");
 			query_time = query_time + (System.currentTimeMillis() - start5);
 			
 //			if(rs.next()){
